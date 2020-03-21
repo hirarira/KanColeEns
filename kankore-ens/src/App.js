c7db75a0a4730f08148000373a5f0f2ce6b32cb8
hirarira <hirarira617@gmail.com>
2020-03-22 01:27:16 +0900

    時分を動的に挿入

--- kankore-ens/src/App.js
+++ kankore-ens/src/App.js
@@ -12,8 +12,13 @@ function App() {
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
+    textBody: {
+      textAlign: 'center'
+    }
   }));
   const classes = useStyles();
+  const hourList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 24];
+  const minutesList = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
   const [enseiHour, setEnseiHour] = React.useState('');
   const [enseiMinutes, setEnseiMinutes] = React.useState('');
   const changeEnseiHour = event => {
@@ -25,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <h2 className="header">艦これ　遠征支援プログラム ver2.0</h2>
-      <Grid container spacing={3} alignItems="center">
+      <Grid container spacing={3} alignItems="center" className={classes.textBody}>
         <Grid item xs={5}>
           遠征に出す時間：
         </Grid>
@@ -38,46 +43,46 @@ function App() {
               value={enseiHour}
               onChange={changeEnseiHour}
             >
-              <MenuItem value={1}>1</MenuItem>
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={6}>6</MenuItem>
-              <MenuItem value={7}>7</MenuItem>
-              <MenuItem value={8}>8</MenuItem>
-              <MenuItem value={9}>9</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={12}>12</MenuItem>
-              <MenuItem value={15}>15</MenuItem>
-              <MenuItem value={20}>20</MenuItem>
-              <MenuItem value={24}>24</MenuItem>
+              {
+                hourList.map((x) =>{
+                  return (
+                    <MenuItem value={x}>{x}</MenuItem>
+                  )
+                })
+              }
             </Select>
           </FormControl>
         </Grid>
         <Grid item xs={3}>
-        <FormControl className={classes.formControl}>
-            <InputLabel id="enseiMinutes">分</InputLabel>
+          <FormControl className={classes.formControl}>
+          <InputLabel id="enseiMinutes">分</InputLabel>
             <Select
               labelId="enseiMinutes"
               id="enseiMinutesSelect"
               value={enseiMinutes}
               onChange={changeEnseiMinutes}
             >
-              <MenuItem value={0}>0</MenuItem>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={15}>15</MenuItem>
-              <MenuItem value={20}>20</MenuItem>
-              <MenuItem value={25}>25</MenuItem>
-              <MenuItem value={30}>30</MenuItem>
-              <MenuItem value={35}>35</MenuItem>
-              <MenuItem value={40}>40</MenuItem>
-              <MenuItem value={45}>45</MenuItem>
-              <MenuItem value={50}>50</MenuItem>
-              <MenuItem value={55}>55</MenuItem>
+              {
+                minutesList.map((x) =>{
+                  return (
+                    <MenuItem value={x}>{x}</MenuItem>
+                  )
+                })
+              }
             </Select>
           </FormControl>
+        </Grid>
+        <Grid item xs={3}>
+          燃料
+        </Grid>
+        <Grid item xs={3}>
+          弾薬
+        </Grid>
+        <Grid item xs={3}>
+          鋼材
+        </Grid>
+        <Grid item xs={3}>
+          ボーキ
         </Grid>
       </Grid>
     </div>
