3cc59053a2a73b0381a7e320e4e2235c9c5f0403
hirarira <hirarira617@gmail.com>
2020-05-11 02:17:40 +0900

    構造を修正

--- kankore-ens/src/lib/Search.js
+++ kankore-ens/src/lib/Search.js
@@ -28,6 +28,7 @@ function Search(props) {
                 <TableRow>
                   <TableCell align="right">No.</TableCell>
                   <TableCell align="right">遠征名</TableCell>
+                  <TableCell align="right">時間</TableCell>
                   <TableCell align="right">Exp</TableCell>
                   <TableCell align="right">燃料</TableCell>
                   <TableCell align="right">弾薬</TableCell>
@@ -43,18 +44,19 @@ function Search(props) {
                 {showNinmu.map((ninmu) => {
                   return (
                     <TableRow key={ninmu.no}>
-                    <TableCell align="right">{ninmu.no}</TableCell>
-                    <TableCell align="right">{ninmu.name}</TableCell>
-                    <TableCell align="right">{ninmu.time}</TableCell>
-                    <TableCell align="right">{ninmu.exp}</TableCell>
-                    <TableCell align="right">{ninmu.fuel}</TableCell>
-                    <TableCell align="right">{ninmu.bullet}</TableCell>
-                    <TableCell align="right">{ninmu.steel}</TableCell>
-                    <TableCell align="right">{ninmu.bauxite}</TableCell>
-                    <TableCell align="right">{ninmu.needHeadLv}</TableCell>
-                    <TableCell align="right">{ninmu.needNum}</TableCell>
-                    <TableCell align="right">{ninmu.remarks}</TableCell>
-                  </TableRow>
+                      <TableCell align="right">{ninmu.no}</TableCell>
+                      <TableCell align="right">{ninmu.name}</TableCell>
+                      <TableCell align="right">{ninmu.time}</TableCell>
+                      <TableCell align="right">{ninmu.exp}</TableCell>
+                      <TableCell align="right">{ninmu.fuel}</TableCell>
+                      <TableCell align="right">{ninmu.bullet}</TableCell>
+                      <TableCell align="right">{ninmu.steel}</TableCell>
+                      <TableCell align="right">{ninmu.bauxite}</TableCell>
+                      <TableCell align="right">{ninmu.needHeadLv}</TableCell>
+                      <TableCell align="right">{ninmu.needNum}</TableCell>
+                      <TableCell align="right">{ninmu.needKanmusu}</TableCell>
+                      <TableCell align="right">{ninmu.remarks}</TableCell>
+                    </TableRow>
                   );
                 })}
               </TableBody>
