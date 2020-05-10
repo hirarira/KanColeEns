311b03a251e1db7d680437b3896580d8de55325b
hirarira <hirarira617@gmail.com>
2020-05-11 02:10:55 +0900

    任務一覧を表示できるようにしました

--- kankore-ens/src/lib/Search.js
+++ kankore-ens/src/lib/Search.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Grid, Button } from '@material-ui/core';
-import ShowResult from './ShowResult'
-import Ninmu from './Ninmu';
+import { Grid, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+import NinmuList from './Ninmu';
 
 function Search(props) {
   const [showNinmu, setShowNinmu] = React.useState([]);
   const filter = () => {
     console.log(props);
-    setShowNinmu();
-    console.log(Ninmu);
+    setShowNinmu(NinmuList);
   }
   return (
     <Grid container spacing={3}>
@@ -17,9 +15,53 @@ function Search(props) {
           検索
         </Button>
       </Grid>
-      <ShowResult
-        list={showNinmu}
-      />
+      { showNinmu.length === 0 &&
+        <Grid item xs={12}>
+          ここに結果が表示されます
+        </Grid>
+      }
+      { showNinmu.length > 0 &&
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table aria-label="result table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">No.</TableCell>
+                  <TableCell align="right">遠征名</TableCell>
+                  <TableCell align="right">Exp</TableCell>
+                  <TableCell align="right">燃料</TableCell>
+                  <TableCell align="right">弾薬</TableCell>
+                  <TableCell align="right">鋼材</TableCell>
+                  <TableCell align="right">ボーキ</TableCell>
+                  <TableCell align="right">旗艦Lv</TableCell>
+                  <TableCell align="right">艦隊数</TableCell>
+                  <TableCell align="right">必要艦</TableCell>
+                  <TableCell align="right">備考</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {showNinmu.map((ninmu) => {
+                  return (
+                    <TableRow key={ninmu.no}>
+                    <TableCell align="right">{ninmu.no}</TableCell>
+                    <TableCell align="right">{ninmu.name}</TableCell>
+                    <TableCell align="right">{ninmu.time}</TableCell>
+                    <TableCell align="right">{ninmu.exp}</TableCell>
+                    <TableCell align="right">{ninmu.fuel}</TableCell>
+                    <TableCell align="right">{ninmu.bullet}</TableCell>
+                    <TableCell align="right">{ninmu.steel}</TableCell>
+                    <TableCell align="right">{ninmu.bauxite}</TableCell>
+                    <TableCell align="right">{ninmu.needHeadLv}</TableCell>
+                    <TableCell align="right">{ninmu.needNum}</TableCell>
+                    <TableCell align="right">{ninmu.remarks}</TableCell>
+                  </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      }
     </Grid>
   )
 }
